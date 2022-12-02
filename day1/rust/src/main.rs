2ff42f97ac4ac82b296cc7e5747c3946342573e6
Dimitar Ganev <d.ganev@protonmail.ch>
2022-12-02 12:07:33 +0200

    Refactor day1

--- day1/rust/src/main.rs
+++ day1/rust/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs::read_to_string};
 
-fn main() -> std::io::Result<()> {
+fn main() {
     let input = read_to_string("../input").unwrap();
     let mut number_elves: Vec<i32> = Vec::new();
     let mut sum = 0;
@@ -13,9 +13,10 @@ fn main() -> std::io::Result<()> {
             sum += i.parse::<i32>().unwrap();
         }
     }
+
     number_elves.sort();
     number_elves.reverse();
     sum = number_elves.iter().take(3).sum();
+    
     println!("{}", sum);
-    Ok(())
 }
