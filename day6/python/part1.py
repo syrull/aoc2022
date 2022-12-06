1a847ca9b97d4bf0577ad8f749909f95ff3220cd
Dimitar Ganev <d.ganev@protonmail.ch>
2022-12-06 10:04:57 +0200

    Add day6 solutions

---
+++ day6/python/part1.py
@@ -0,0 +1,18 @@
+def import_input():
+    string = None
+    with open("../input", "r") as input_file:
+        string = input_file.read()
+    return string
+
+def parse_input(string: str):
+    distinct_chars = 4
+    for i in range(0, len(string)):
+        uc = set()
+        chunk = string[i:i+distinct_chars]
+        for s in chunk:
+            uc.add(s)
+        if len(uc) == distinct_chars:
+            return i + distinct_chars
+
+input_ = import_input()
+print(parse_input(input_))
